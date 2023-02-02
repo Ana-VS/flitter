0fde5fb88eb2137a476ebd729dab6c39555de7be
unknown <mangeles.cg91@gmail.com>
2023-02-02 13:48:46 +0100

    primer modelo de vistas

--- src/router/index.ts
+++ src/router/index.ts
@@ -2,23 +2,41 @@ import { createRouter, createWebHashHistory, RouteRecordRaw } from "vue-router";
 import HomeView from "../views/HomeView.vue";
 
 const routes: Array<RouteRecordRaw> = [
+    // PublicView
     {
         path: "/",
         name: "home",
         component: HomeView,
     },
-
-    // {
-    //     path: "/login",
-    //     name: "login",
-    //     component: () =>
-    //         import(/* webpackChunkName: "about" */ "../views/LoginView.vue"),
-    // },
+    {
+        path: "/login",
+        name: "login",
+        component: () =>
+            import(/* webpackChunkName: "about" */ "../views/LoginView.vue"),
+    },
+    {
+        path: "/privateView",
+        name: "privateView",
+        component: () =>
+            import(/* webpackChunkName: "about" */ "../views/PrivateView.vue"),
+    },
+    {
+        path: "/profile",
+        name: "profile",
+        component: () =>
+            import(/* webpackChunkName: "about" */ "../views/ProfileView.vue"),
+    },
     {
         path: "/about",
         name: "about",
         component: () =>
             import(/* webpackChunkName: "about" */ "../views/AboutView.vue"),
+    },
+    {
+        path: "/:pathMatch(.*)",
+        component: import(
+            /*webpackChunkName: notFound*/ "../views/NotFound.vue"
+        ),
     },
 ];
 

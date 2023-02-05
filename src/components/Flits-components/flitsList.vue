37e251dbe8869c8c57ca2bb550dd3e063b93d780
unknown <mangeles.cg91@gmail.com>
2023-02-05 14:47:57 +0100

    Componente de FlitsList

--- src/components/Flits-components/flitsList.vue
+++ src/components/Flits-components/flitsList.vue
@@ -1,11 +1,29 @@
-<template></template>
+<template>
+    <div v-if="isLoading">Loading...</div>
+    <div v-else>
+        <div class="flitList">
+            <flitBox v-for="flit in flits" :key="flit.id" :flit="flit" />
+        </div>
+    </div>
+</template>
 
 <script lang="ts">
 import { defineComponent } from "vue";
 import flitBox from "./flitBox.vue";
+import useFlits from "@/composables/useFlits";
 
 export default defineComponent({
-    setup() {},
+    name: "flitsList",
+    components: {
+        flitBox,
+    },
+    setup() {
+        const { isLoading, flits } = useFlits();
+        return {
+            isLoading,
+            flits,
+        };
+    },
 });
 </script>
 
